a1f3cd5506c06bf112785caa5d2f73035061f63d
William Nguyen <105384182+wnguye03@users.noreply.github.com>
2023-01-03 15:32:17 -0800

    Partially finished homepage, still need to finish posting mechanism

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,99 @@
 import 'package:flutter/material.dart';
+import 'colorScheme.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(GemzHomepage());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class GemzHomepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+        //issue with theme colouring and needs to be fixed later
+        theme: ThemeData(primaryColor: Colors.black),
+        home: Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+                icon: const Icon(Icons.list),
+                //**placeholder on press function
+                onPressed: () => {}),
+            title: const Text('GEMZ'),
+            actions: <Widget>[
+              IconButton(icon: const Icon(Icons.person), onPressed: () => {})
+            ],
+          ),
+          body: Container(
+              padding: const EdgeInsets.only(left: 15, right: 15),
+              color: Colors.black,
+              child: Column(
+                children: const [
+                  Divider(
+                    color: Colors.white,
+                    height: 5,
+                    thickness: 3,
+                    indent: 25,
+                    endIndent: 25,
+                  ),
+                  Divider(
+                    color: Colors.blueAccent,
+                    height: 5,
+                    thickness: 3,
+                    indent: 25,
+                    endIndent: 25,
+                  ),
+                  Padding(
+                      padding: EdgeInsets.only(top: 90),
+                      child: Text("Haven't posted yet?",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30))),
+                  Padding(
+                      padding: EdgeInsets.only(left: 20, top: 15, right: 20),
+                      child: Text(
+                          "Share a quick gem of content today so that you can connect and explore with the rest!",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20))),
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+                //TODO: Finish Posting mechanism
+                  // Padding(
+                  //     padding: EdgeInsets.only(top: 90),
+                  //     child: Form(
+                  //       key: _postKey,
+                  //       child: Column(
+                  //         crossAxisAlignment: CrossAxisAlignment.start,
+                  //         children: <Widget>[
+                  //           Text("Share your Gemz"),
+                  //           TextFormField(
+                  //             decoration: const InputDecoration(
+                  //               hintText: "Original Thought",
+                  //             ),
+                  //             validator: (String? value) {
+                  //               if (value == null || value.isEmpty) {
+                  //                 return 'Please enter your Gemz!';
+                  //               }
+                  //               return null;
+                  //             },
+                  //           ),
+                  //           Padding(
+                  //             padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  //             child: ElevatedButton(
+                  //               onPressed: () => {},
+                  //               child: const Text("SHARE"),
+                  //            ),
+                  //           ),
+                  //         ],
+                  //       ),
+                  //     )),
+                  Padding(
+                      padding: EdgeInsets.all(30),
+                      child: Text(
+                          "Tip: Keep a list of your favourite links all in one place so that you've always got new Gemz to share!",
+                          style: TextStyle(
+                              color: Colors.yellowAccent,
+                              fontWeight: FontWeight.bold))),
+                ],
+              )),
+        ));
   }
 }
